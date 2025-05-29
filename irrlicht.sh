1184a3435163e2cb9639a9e6ca46b94cf4e49242
Deve <deveee@gmail.com>
2025-05-29 22:02:31 +0200

    Build irrlicht with SFML

--- irrlicht.sh
+++ irrlicht.sh
@@ -6,18 +6,18 @@ mkdir -p output/irrlicht/lib/$TARGET_ABI
 mkdir -p deps; cd deps
 
 [ ! -d irrlicht-src ] && \
-	git clone --depth 1 -b SDL2 https://github.com/MoNTE48/Irrlicht irrlicht-src
+	git clone --depth 1 -b SFML https://github.com/deveee/Irrlicht irrlicht-src
 
-cd irrlicht-src/source/Irrlicht/Android-SDL2
+cd irrlicht-src/source/Irrlicht/Android-SFML
 
-export SDL2_PATH="$OUTPUT_PATH/sdl2/"
+export SFML_PATH="$OUTPUT_PATH/sfml/"
 $ANDROID_NDK/ndk-build -j \
 	NDEBUG=1 \
 	APP_ABI="$TARGET_ABI" \
 	APP_PLATFORM=android-"$API" \
 	APP_CFLAGS="$CFLAGS -I$OUTPUT_PATH/openssl/include" \
 	APP_CXXFLAGS="$CXXFLAGS -std=gnu++17" \
-	APP_CPPFLAGS="$APP_CXXFLAGS -DNO_IRR_COMPILE_WITH_SDL_TEXTINPUT_ -I$OUTPUT_PATH/libjpeg/include -I$OUTPUT_PATH/libpng/include" \
+	APP_CPPFLAGS="$APP_CXXFLAGS -I$OUTPUT_PATH/libjpeg/include -I$OUTPUT_PATH/libpng/include" \
 	APP_STL="c++_static"
 
 # update `include` folder
@@ -25,7 +25,7 @@ rm -rf ../../../../../output/irrlicht/include
 cp -r ../../../include ../../../../../output/irrlicht/include
 # update lib
 rm -rf ../../../../../../../Irrlicht/lib/$TARGET_ABI/libIrrlicht.a
-cp -r ../../../lib/Android-SDL2/$TARGET_ABI/libIrrlicht.a ../../../../../output/irrlicht/lib/$TARGET_ABI/libIrrlicht.a
+cp -r ../../../lib/Android-SFML/$TARGET_ABI/libIrrlicht.a ../../../../../output/irrlicht/lib/$TARGET_ABI/libIrrlicht.a
 # update shaders
 rm -rf ../../../../../output/irrlicht/shaders
 cp -r ../../../media/Shaders ../../../../../output/irrlicht/shaders
